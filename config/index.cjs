33922527d1a1e394b084c1d7ae37606a56b45079
nihaoshi <108377994+nihaoshi@users.noreply.github.com>
2024-07-10 10:12:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ou0Fl6Tvd_Jc_dEyDtGv09aybwIo',
+      id: '98f8d931844b49f99be4bb95c2d9752a',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
